e51b1672dc26eff47efaace91aaefd84c5168df7
lhendrickson3@my.wctc.edu <>
2013-09-09 15:42:11 -0500

    Created the GameGUI and the Runner

---
+++ src/lab2/Runner.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author User
+ */
+public class Runner {
+    public static void main(String[] args) {
+        
+    }
+}
