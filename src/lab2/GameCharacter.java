20596709d842700b0d415f89b93dd47e447fa2ad
lhendrickson3@my.wctc.edu <>
2013-09-09 15:57:46 -0500

    Added private variables with public setters and getters in the GameCharacter and some action methods

--- src/lab2/GameCharacter.java
+++ src/lab2/GameCharacter.java
@@ -9,5 +9,52 @@ package lab2;
  * @author User
  */
 public class GameCharacter {
+    private String gender;
+    private String race;
+    private String characterClass;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getCharacterClass() {
+        return characterClass;
+    }
+
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
     
+    public void autoAttack(){
+        
+    }
+    
+    public void castSpell(){
+        
+    }
+    
+    public void takeDamage(){
+        calculateDamage();
+        decrementHealth(10);
+    }
+    
+    public void calculateDamage(){
+        System.out.println("calculated damage");
+    }
+    
+    public void decrementHealth(int damage){
+        System.out.println("removed health");
+    }
 }
